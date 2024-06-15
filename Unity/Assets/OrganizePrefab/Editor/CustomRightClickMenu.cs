59019366d6223dc1b34e31ca9400a6a38667270f
xuyin <axuyin@gmail.com>
2024-06-15 13:55:12 +0800

    添加 整理 prefab的工具,用于将prefab中所有引用的资源和prefab复制到新文件夹中

---
+++ Unity/Assets/OrganizePrefab/Editor/CustomRightClickMenu.cs
@@ -0,0 +1,66 @@
+using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class CustomRightClickMenu: MonoBehaviour
+{
+    
+    [MenuItem("Assets/复制此Prefab",false,10)]
+    static void MyTestA()
+    {
+        var obj=    Selection.activeObject as GameObject;
+        GetResourcePaths(obj);
+    }
+
+    public static void GetResourcePaths(GameObject prefab)
+    {
+        string targetDirectory; // 目标文件夹路径
+        // 获取 Prefab 的路径
+        string prefabPath = AssetDatabase.GetAssetPath(prefab);
+        // 检查目标文件夹是否存在，如果不存在则创建
+        // 复制资源文件到目标文件夹
+        var oo = Application.dataPath.Replace("Assets", "");
+      ;
+        targetDirectory =Path.Combine(oo+  Path.GetDirectoryName(prefabPath), "New");
+        if (Directory.Exists( targetDirectory))
+        {
+            Directory.Delete(targetDirectory, true);
+          
+        }
+        Directory.CreateDirectory(targetDirectory);
+
+        // 获取 Prefab 的所有依赖项
+        string[] dependencies = AssetDatabase.GetDependencies(prefabPath);
+
+        // 打印所有依赖项的路径
+        foreach (string dependency in dependencies)
+        {
+            if (dependency.EndsWith(".cs"))continue;
+            // 检查资源路径是否为空
+            if (!string.IsNullOrEmpty(dependency))
+            {
+                // 获取资源文件名
+                string fileName = Path.GetFileName(dependency);
+
+                // 生成目标文件路径
+                string targetPath = Path.Combine(targetDirectory, fileName);
+
+                // 复制资源文件到目标文件夹
+             
+                
+                File.Copy(Path.Combine(oo,dependency),Path.Combine(oo,targetPath), true); // true表示如果目标文件已存在，则覆盖它
+                
+                
+                string oldfile=Path.Combine(oo,dependency+".meta");
+                string newfile=Path.Combine(oo,targetPath+".meta");
+                File.Copy(oldfile,newfile, true); // true表示如果目标文件已存在，则覆盖它
+                Debug.Log($"复制资源: {dependency} 到 {targetPath}");
+            }
+        }
+        
+        AssetDatabase.Refresh();
+    }
+
+
+}
